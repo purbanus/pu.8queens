d69ac9e0576c4e8c3f8a6ca56b4329c34d846f19
peter <purbanus@gmail.com>
2024-12-01 23:54:19 +0100

    Afdrukken met MatrixFormatter

--- pu.8queens/src/main/java/pu/eightqueens/MainMetBitSet.java
+++ pu.8queens/src/main/java/pu/eightqueens/MainMetBitSet.java
@@ -1,25 +1,38 @@
 package pu.eightqueens;
 
+import java.util.List;
+
+import pu.services.MatrixFormatter;
+import pu.services.MatrixFormatter.Alignment;
 import pu.services.StopWatch;
+import pu.services.StringHelper;
 
 public class MainMetBitSet
 {
 public static void main( String [] args )
 {
-	for ( int rows = 4; rows <= 13; rows++ )
+	MatrixFormatter matrixFormatter = new MatrixFormatter();
+	matrixFormatter.setDefaultAlignment( MatrixFormatter.ALIGN_RIGHT );
+	matrixFormatter.addHeader( StringHelper.repChar( '-', 50 ) );
+	matrixFormatter.addDetail( new String [] { "Zijde", "Aantal Oplossingen", "Tijd" } );
+	matrixFormatter.addHeader( StringHelper.repChar( '-', 50 ) );
+	for ( int rows = 1; rows <= 15; rows++ )
 	{
-		new MainMetBitSet().run( rows );
+		new MainMetBitSet().run( rows, matrixFormatter );
 	}
+	matrixFormatter.addHeader( StringHelper.repChar( '-', 50 ) );
+	System.out.println( matrixFormatter.getOutput() );
 }
 private int numberOfSolutions = 0;
 private Board board;
 
-public void run( int aSquareBoardSize )
+public void run( int aSquareBoardSize, MatrixFormatter aMatrixFormatter )
 {
 	StopWatch timer = new StopWatch();
 	board = new BoardMetBitSet( aSquareBoardSize, aSquareBoardSize );
 	runRow( 0, board );
-	System.out.println( "Finished! Aantal oplossingen: " + numberOfSolutions + " time used: " + timer.getElapsedMs() );
+	//System.out.println( "Zijde = " + aSquareBoardSize + " Aantal oplossingen: " + numberOfSolutions + " time used: " + timer.getElapsedMs() );
+	aMatrixFormatter.addDetail( aSquareBoardSize + "\t" + numberOfSolutions + "\t" + timer.getElapsed() );
 }
 public void runRow( int aRow, Board aBoard )
 {
